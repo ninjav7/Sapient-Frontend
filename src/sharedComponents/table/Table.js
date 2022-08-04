d7e472d6c59a9981063909ea8505779d8078d43a
Igor Kuryakov <remrtehno@gmail.com>
2022-08-04 12:27:04 +0500

    feat: Add Table Component

---
+++ src/sharedComponents/table/Table.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import './Table.scss';
+
+const Head = ({ children, ...props }) => (
+    <thead className="table-head" {...props}>
+        {children}
+    </thead>
+);
+const Body = ({ children, ...props }) => (
+    <tbody className="table-body" {...props}>
+        {children}
+    </tbody>
+);
+const Row = ({ children, ...props }) => (
+    <tr className="table-row" {...props}>
+        {children}
+    </tr>
+);
+const Cell = ({ children, ...props }) => (
+    <td className="table-cell" {...props}>
+        {children}
+    </td>
+);
+
+const THead = ({ children, ...props }) => (
+    <Head {...props}>
+        <Row>{children}</Row>
+    </Head>
+);
+const TBody = ({ children, ...props }) => <Body {...props}>{children}</Body>;
+
+const Table = ({ children, ...props }) => {
+    return (
+        <div className="table-wrapper" {...props}>
+            <table className="table-component">{children}</table>
+        </div>
+    );
+};
+
+Table.Head = Head;
+Table.Body = Body;
+Table.Row = Row;
+Table.Cell = Cell;
+Table.THead = THead;
+Table.TBody = TBody;
+
+export default Table;
