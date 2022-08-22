2654c165c0b63eab9dbe8f08766e40f076721ede
Igor Kuryakov <remrtehno@gmail.com>
2022-08-22 14:56:31 +0500

    refactor: Clean value for TinyBarChart

--- src/sharedComponents/tinyBarChart/TinyBarChart.js
+++ src/sharedComponents/tinyBarChart/TinyBarChart.js
@@ -5,10 +5,18 @@ import { mixColors } from '../helpers/helper';
 
 import './TinyBarChart.scss';
 
+const cleanPercentage = percent => {
+    if (Number.isNaN(percent) || percent === 'NaN') {
+        return 0;
+    }
+
+    return percent > 100 ? 100 : percent || 0;
+};
+
 const TinyBarChart = ({ percent, ...props }) => {
     const stylesLine = {
-        width: `${percent || 0}%`,
-        backgroundColor: mixColors('F63D68', '5F8EFC', percent),
+        width: `${cleanPercentage(percent)}%`,
+        backgroundColor: mixColors('F63D68', '5F8EFC', cleanPercentage(percent)),
     };
 
     return (
