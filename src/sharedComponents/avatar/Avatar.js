468485b9d2d48e1542c409e833d46534438af357
Igor Kuryakov <remrtehno@gmail.com>
2022-08-31 13:20:17 +0500

    feat: Add Avatar Component

---
+++ src/sharedComponents/avatar/Avatar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+
+import Typography from '../typography';
+
+import './Avatar.scss';
+
+const AVATAR_SIZES = Object.freeze({
+    sm: 'sm',
+    md: 'md',
+    lg: 'lg',
+    xl: 'xl',
+});
+
+const Avatar = props => {
+    const classNames = cx('Avatar-wrapper', {
+        [props.size]: !!props.size,
+    });
+
+    return (
+        <div className={classNames}>
+            <div className="Avatar-image-wrapper">
+                <img src={props.avatarUrl} alt="avatar image" className="Avatar-image" />
+                {/* Indicator was removed for awhile. */}
+                {/*<div className="Avatar-indicator"></div>*/}
+            </div>
+
+            <div className="Avatar-details">
+                <Typography.Body size={Typography.Sizes.sm} className="Avatar-details-userName">
+                    {props.userName}
+                </Typography.Body>
+                <Typography.Body size={Typography.Sizes.xs} className="userEmail">
+                    {props.userEmail}
+                </Typography.Body>
+            </div>
+        </div>
+    );
+};
+
+Avatar.Sizes = AVATAR_SIZES;
+
+Avatar.propTypes = {
+    avatarUrl: PropTypes.string.isRequired,
+    userName: PropTypes.string.isRequired,
+    userEmail: PropTypes.string.isRequired,
+    size: PropTypes.oneOf(Object.values(AVATAR_SIZES)),
+};
+
+Avatar.defaultProps = {
+    size: AVATAR_SIZES.sm,
+};
+
+export default Avatar;
