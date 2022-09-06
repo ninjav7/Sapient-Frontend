fd77d30cf8f9709a30cb6ee17e6ae2d58c810015
sudhanshu_rai <sudhanshurai@zoof.co.in>
2022-09-06 20:05:35 +0530

    Base URL updated for Staging Environment.

--- src/services/Network.js
+++ src/services/Network.js
@@ -1,5 +1,5 @@
-export const BaseUrl = 'https://energy-service-dev.azurewebsites.net';
-// export const BaseUrl = 'https://energy-service-dev-internal.azurewebsites.net';
+// export const BaseUrl = 'https://energy-service-dev.azurewebsites.net';
+export const BaseUrl = 'https://energy-service-dev-internal.azurewebsites.net';
 // export const BaseUrl = 'http://122.175.198.21:8000';
 
 export const portfolioOverall = '/api/energy/portfolio/overall';
