11cbc4e3b24a0e3c8ee4c9115dc6c9a826511e16
sudhanshu_rai <sudhanshurai@zoof.co.in>
2022-09-16 01:31:13 +0530

    EM-593: Mouseover standardization on charts added.

--- src/pages/charts/BrushChart.js
+++ src/pages/charts/BrushChart.js
@@ -2,20 +2,26 @@ import React, { useState, useEffect } from 'react';
 import Chart from 'react-apexcharts';
 import ReactApexChart from 'react-apexcharts';
 import { Card, CardBody } from 'reactstrap';
+import Brick from '../../sharedComponents/brick';
+import '../../sharedComponents/lineChartWidget/style.scss';
 
 // stacked bar chart
-const BrushChart = ({ optionsData = {}, seriesData = [], optionsLineData = {}, seriesLineData = [] }) => {
-    
+const BrushChart = ({
+    optionsData = {},
+    seriesData = [],
+    optionsLineData = {},
+    seriesLineData = [],
+    className = '',
+}) => {
     return (
-        <Card style={{width:"95%"}}>
+        <Card style={{ width: '95%' }}>
             <CardBody>
                 <div id="wrapper">
-                    <div id="chart-line2">
-                        {/* <ReactApexChart options={options} series={series} type="line" height={230} /> */}
+                    <div id="chart-line2" className={`line-chart-widget-wrapper ${className}`}>
+                        <Brick sizeInRem={1} />
                         <ReactApexChart options={optionsData} series={seriesData} type="line" height={230} />
                     </div>
                     <div id="chart-line">
-                        {/* <ReactApexChart options={optionsLine} series={seriesLine} type="area" height={130} /> */}
                         <ReactApexChart options={optionsLineData} series={seriesLineData} type="area" height={130} />
                     </div>
                 </div>
