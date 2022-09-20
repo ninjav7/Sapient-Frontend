6f5c7a9d85e1d33d6785ede7ec944eb95ee94705
sudhanshu_rai <sudhanshurai@zoof.co.in>
2022-09-21 00:08:04 +0530

    EM-369 : Component for Model Headers created.

--- src/store/BuildingStore.js
+++ src/store/BuildingStore.js
@@ -3,7 +3,10 @@ import { Store } from 'pullstate';
 export const BuildingStore = new Store({
     BldgId: localStorage.getItem('buildingId'),
     BldgName: localStorage.getItem('buildingName'),
-    BldgTimeZone: localStorage.getItem('buildingTimeZone'),
+    BldgTimeZone:
+        localStorage.getItem('buildingTimeZone') === 'undefined'
+            ? 'US/Eastern'
+            : localStorage.getItem('buildingTimeZone'),
 });
 
 export const BuildingListStore = new Store({
