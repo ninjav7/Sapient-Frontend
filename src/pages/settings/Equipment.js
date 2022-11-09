70c18a374b043831af9ea3268a48ba7306435192
sudhanshurai28 <sudhanshu.rai@sapient.industries>
2022-11-10 00:44:29 +0530

    Equip API Hotfixes.

--- src/pages/settings/Equipment.js
+++ src/pages/settings/Equipment.js
@@ -807,7 +807,7 @@ const Equipment = () => {
                 Authorization: `Bearer ${userdata.token}`,
             };
             let params = `&building_id=${bldgId}`;
-            await axios.get(`${BaseUrl}${path}${params}`, { headers }).then((res) => {
+            await axios.post(`${BaseUrl}${path}${params}`, {}, { headers }).then((res) => {
                 let responseData = res.data;
                 setPaginationData(res.data);
                 setGeneralEquipmentData(responseData.data);
@@ -844,7 +844,7 @@ const Equipment = () => {
                 Authorization: `Bearer ${userdata.token}`,
             };
             let params = `&building_id=${bldgId}`;
-            await axios.get(`${BaseUrl}${path}${params}`, { headers }).then((res) => {
+            await axios.post(`${BaseUrl}${path}${params}`, {}, { headers }).then((res) => {
                 let responseData = res.data;
                 setPaginationData(res.data);
                 setGeneralEquipmentData(responseData.data);
