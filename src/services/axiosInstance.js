6ca34322d8b7ca188930ac09c0882f00b48b12e9
Oleksii-Poletaiev <oleksii.poletaiev@sapient.industries>
2022-11-28 13:24:58 +0100

    PLT-420 add filters for equipment page

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 import {BaseUrl} from './Network';
 import { Cookies } from 'react-cookie';
 let cookies = new Cookies();
-
 let userdata = cookies.get('user');
-
 const defaultOptions = {
   baseURL: BaseUrl,
   Authorization: `Bearer ${userdata?.token}`,
@@ -18,6 +16,10 @@ const defaultOptions = {
 const axiosInstance = axios.create(defaultOptions);
 
 axiosInstance.interceptors.request.use((config) => {
+  let cookies = new Cookies();
+  let userdata = cookies.get('user');
+  config.headers.Authorization = `Bearer ${userdata?.token}`;
+
 
   return config;
 });
