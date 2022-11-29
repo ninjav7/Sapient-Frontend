222627713bef7bd632099efa0a418a283fa574f9
Abhijeet <abhijeet.patil@sapient.industries>
2022-11-29 12:41:28 +0530

    Control Changes Reverted

--- src/services/plugRules.js
+++ src/services/plugRules.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import axiosInstance from './axiosInstance';
 import {
     listPlugRules,
@@ -8,15 +7,13 @@ import {
     plugRuleDetails,
     graphData,
     getListSensorsForBuildings,
-    assignSensorsToRule,
+    linkSocket,
     listLinkSocketRules,
     unLinkSocket,
-    getFiltersForSensors,
-    getSensorLastUsed,
 } from './Network';
 
-export function fetchPlugRules(activeBuildingId, params) {
-    return axiosInstance.get(`${listPlugRules}?building_id=${activeBuildingId}`, params).then((res) => {
+export function fetchPlugRules(activeBuildingId) {
+    return axiosInstance.get(`${listPlugRules}?building_id=${activeBuildingId}`).then((res) => {
         return res;
     });
 }
@@ -48,60 +45,20 @@ export function deletePlugRuleRequest(ruleId) {
     });
 }
 
-export function getGraphDataRequest(activeBuildingId, sensorsIdNow, plugRuleId) {
+export function getGraphDataRequest(activeBuildingId, sensorsIdNow) {
     let params = `?building_id=${activeBuildingId}&sensors=${sensorsIdNow}`;
-    return axiosInstance
-        .get(`${graphData}${params}`, {
-            params: {
-                //@TODO Hardcoded because it doesn't have default values on backend side, but we don't need them right now.
-                tz_info: 'US/Eastern',
-                num_of_days: 50,
-                plug_rule_id: plugRuleId,
-            },
-        })
-        .then((res) => {
-            return res;
-        });
-}
-
-export function getListSensorsForBuildingsRequest(page_size, pageNo, ruleId, activeBuildingId, getParams) {
-    let params = `?page_size=${page_size}&page_no=${pageNo}&rule_id=${ruleId}&building_id=${activeBuildingId}`;
-
-    if (page_size === 0) {
-        return;
-    }
-
-    return axiosInstance.get(`${getListSensorsForBuildings}${params}`, { params: getParams }).then((res) => {
+    return axiosInstance.get(`${graphData}${params}`).then((res) => {
         return res;
     });
 }
 
-/**
- * Request filters.
- * @param args = {sensor_search, equipment_types, mac_address, tags, floor_id, space_id, space_type_id, sensor_number};
- * @returns {Promise<AxiosResponse<any>>}
- */
-export function getFiltersForSensorsRequest(args) {
-    return axiosInstance
-        .get(`${getFiltersForSensors}`, {
-            params: _.pickBy(
-                {
-                    building_id: args.activeBuildingId,
-                    mac_address: args.macTypeFilterString,
-                    equipment_types: args.equpimentTypeFilterString,
-                    sensor_number: args.sensorTypeFilterString,
-                    floor_id: args.floorTypeFilterString,
-                    space_id: args.spaceTypeFilterString,
-                    space_type_id: args.spaceTypeTypeFilterString,
-                },
-                _.identity
-            ),
-        })
-        .then((res) => {
-            return res.data;
-        });
-}
+export function getListSensorsForBuildingsRequest(totalSocket, pageNo, ruleId, activeBuildingId) {
+    let params = `?page_size=${totalSocket}&page_no=${pageNo}&rule_id=${ruleId}&building_id=${activeBuildingId}`;
 
+    return axiosInstance.get(`${getListSensorsForBuildings}${params}`).then((res) => {
+        return res;
+    });
+}
 
 export function getUnlinkedSocketRules(
     pageSize,
@@ -111,49 +68,22 @@ export function getUnlinkedSocketRules(
     equpimentTypeFilterString,
     macTypeFilterString,
     locationTypeFilterString,
-    sensorTypeFilterString,
-    floorTypeFilterString,
-    spaceTypeFilterString,
-    spaceTypeTypeFilterString,
-    getParams
+    sensorTypeFilterString
 ) {
-    let params = `?page_size=${pageSize}&page_no=${pageNo}&rule_id=${ruleId}&building_id=${activeBuildingId}&equipment_types=${encodeURIComponent(
-        equpimentTypeFilterString
-    )}&location=${locationTypeFilterString}&sensor_number=${encodeURIComponent(sensorTypeFilterString)}`;
+    let params = `?page_size=${pageSize}&page_no=${pageNo}&rule_id=${ruleId}&building_id=${activeBuildingId}&equipment_types=${equpimentTypeFilterString}&mac_address=${macTypeFilterString}&location=${locationTypeFilterString}&sensor_number=${sensorTypeFilterString}`;
 
-    if (pageSize === 0) {
-        return;
-    }
-
-    return axiosInstance
-        .get(`${getListSensorsForBuildings}${params}`, {
-            params: _.pickBy(
-                {
-                    floor_id: floorTypeFilterString,
-                    space_id: spaceTypeFilterString,
-                    space_type_id: spaceTypeTypeFilterString,
-                    mac_address: macTypeFilterString,
-                    ...getParams,
-                },
-                _.identity
-            ),
-        })
-        .then((res) => {
-            return res;
-        });
-}
-
-export function linkSensorsToRuleRequest(rulesToLink) {
-    return axiosInstance.post(`${assignSensorsToRule}`, rulesToLink).then((res) => {
+    return axiosInstance.get(`${getListSensorsForBuildings}${params}`).then((res) => {
         return res;
     });
 }
-export function listLinkSocketRulesRequest(ruleId, activeBuildingId, sortBy) {
-    let params = `?rule_id=${ruleId}&building_id=${activeBuildingId}`;
 
-    if (sortBy?.method && sortBy?.name) {
-        params += `&order_by=${sortBy.name}&sort_by=${sortBy.method}`;
-    }
+export function linkSocketRequest(rulesToLink) {
+    return axiosInstance.post(`${linkSocket}`, rulesToLink).then((res) => {
+        return res;
+    });
+}
+export function listLinkSocketRulesRequest(ruleId, activeBuildingId) {
+    let params = `?rule_id=${ruleId}&building_id=${activeBuildingId}`;
 
     return axiosInstance.get(`${listLinkSocketRules}${params}`).then((res) => {
         return res;
