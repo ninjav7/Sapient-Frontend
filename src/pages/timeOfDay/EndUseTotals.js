0a302d7f6c762330f49638fed71d86974849cfa2
sudhanshurai28 <sudhanshu.rai@sapient.industries>
2023-02-01 00:18:00 +0530

    Time of Day Chart Black issue fixes.

--- src/pages/timeOfDay/EndUseTotals.js
+++ src/pages/timeOfDay/EndUseTotals.js
@@ -7,11 +7,11 @@ import { TRENDS_BADGE_TYPES } from '../../sharedComponents/trendsBadge';
 
 const EndUseTotals = ({ energyConsumption, isEndUsageChartLoading, className = '' }) => {
     const donutChartData = energyConsumption.map(({ device: label, after_hours_energy_consumption }) => {
-        let val = parseInt(after_hours_energy_consumption.now);
+        let val = parseInt(after_hours_energy_consumption?.now);
         let value = parseFloat(val);
-        const trendValue = percentageHandler(after_hours_energy_consumption.now, after_hours_energy_consumption.old);
+        const trendValue = percentageHandler(after_hours_energy_consumption?.now, after_hours_energy_consumption?.old);
         const trendType =
-            after_hours_energy_consumption.now <= after_hours_energy_consumption.old
+            after_hours_energy_consumption?.now <= after_hours_energy_consumption?.old
                 ? TRENDS_BADGE_TYPES.DOWNWARD_TREND
                 : TRENDS_BADGE_TYPES.UPWARD_TREND;
 
