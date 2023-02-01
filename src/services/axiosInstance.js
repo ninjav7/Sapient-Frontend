471a24be25867b9579aaf7d6d202a06871f33e49
Abhijeet <abhijeet.patil@sapient.industries>
2023-02-01 13:20:31 +0530

    SuperUser Logout Fixes

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -33,7 +33,7 @@ axiosInstance.interceptors.response.use(
         if (error?.response?.status === 403) {
             localStorage.clear();
             cookies.remove('user', { path: '/account/login' });
-            window.location.reload();
+            window.open('/account/login', '_self');
             UserStore.update((s) => {
                 s.showNotification = true;
                 s.notificationMessage = 'Token expired / invalid. Please login again!';
@@ -41,6 +41,19 @@ axiosInstance.interceptors.response.use(
             });
             return;
         }
+        if (userdata === undefined) {
+            // not logged in so redirect to login page with the return url
+            localStorage.clear();
+            cookies.remove('user', { path: '/account/login' });
+            window.open('/account/login', '_self');
+            UserStore.update((s) => {
+                s.showNotification = true;
+                s.notificationMessage = 'Token expired / invalid. Please login again!';
+                s.notificationType = 'error';
+            });
+            return;
+        }
+
         return error?.response;
     }
 );
