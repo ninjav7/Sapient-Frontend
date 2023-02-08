5349b4ef4159a65b2ce78a94d835c7c0945d16fa
Igor Kuryakov <remrtehno@gmail.com>
2023-02-08 17:38:35 +0500

    refactor(PLT-914): linker issues

--- src/sharedComponents/widgets/panel/components/BreakerLinkerColumn.js
+++ src/sharedComponents/widgets/panel/components/BreakerLinkerColumn.js
@@ -1,31 +1,17 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import cx from 'classnames';
 import _ from 'lodash';
 
 import { BreakerLinkerButton } from './BreakerLinkerButton';
 
 const BreakerLinkerColumn = React.memo(({ columnIndex, edge, onBreakerLinkedClick, nodesMap }) => {
-    const countItemsRefAPI = useRef(0);
     const [_, edgesData] = edge;
     const side = columnIndex % 2 === 0 ? 'left' : 'right';
 
     return (
         <div className={cx('breaker-linker-column', `breaker-linker-column-${side}`)}>
             {edgesData.map((edgeData) => {
-                let isLinked = nodesMap[edgeData.source].is_linked && nodesMap[edgeData.target].is_linked;
-                
-                //@TODO Hack, should be resolved by another approach
-                if(isLinked) {
-                    if(countItemsRefAPI.current === 2) {
-                        countItemsRefAPI.current = 0;
-                        isLinked = false;
-                    } else {
-                        countItemsRefAPI.current += 1;
-                    }
-                    
-                } else {
-                    countItemsRefAPI.current = 0;
-                }
+                const isLinked = nodesMap[edgeData.source].is_linked && nodesMap[edgeData.target].is_linked && nodesMap[edgeData.target].parent_breaker ;
                 
                 return (
                     <BreakerLinkerButton
