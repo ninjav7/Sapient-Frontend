b0c6971bc44b3f6bc5f1fc4bc502a18114914ffd
Abhijeet <abhijeet.patil@sapient.industries>
2023-02-22 14:54:27 +0530

    PLT-551-Layout-Cleanup

---
+++ src/pages/settings/layout/services.js
@@ -0,0 +1,12 @@
+import axiosInstance from '../../../services/axiosInstance';
+import {
+    createFloors,
+    getFloors,
+    getLayouts,
+    getSpaces,
+    getSpaceTypes,
+    createSpace,
+    updateSpace,
+    deleteSpace,
+    deleteFloor,
+} from '../../../services/Network';
