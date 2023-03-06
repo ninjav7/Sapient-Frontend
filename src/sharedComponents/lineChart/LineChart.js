3f93b99cd9fd1f6bf4f9f011c5536f8c8ab670ac
Oleksii-Poletaiev <oleksii.poletaiev@sapient.industries>
2023-03-06 13:24:12 +0100

    implement new create flow

--- src/sharedComponents/lineChart/LineChart.js
+++ src/sharedComponents/lineChart/LineChart.js
@@ -73,6 +73,7 @@ const LineChart = (props) => {
         plotBandsLegends,
         isLoadingData,
         unitInfo,
+        chartProps
     } = props;
 
     const [plotBands, setPlotBands] = useState(plotBandsProp);
@@ -202,6 +203,19 @@ const LineChart = (props) => {
         }),
         []
     );
+    const chartConfig = _.merge(
+        options({
+            data,
+            dateRange,
+            Highcharts,
+            tooltipUnit,
+            tooltipLabel,
+            widthOfWrapper,
+            plotBands,
+        }),
+        chartProps
+    );
+    console.log("chartConfig",chartConfig);
 
     return (
         <div className="line-chart-wrapper" ref={wrapperRef}>
@@ -258,15 +272,7 @@ const LineChart = (props) => {
                     <HighchartsReact
                         highcharts={Highcharts}
                         constructorType={'stockChart'}
-                        options={options({
-                            data,
-                            dateRange,
-                            Highcharts,
-                            tooltipUnit,
-                            tooltipLabel,
-                            widthOfWrapper,
-                            plotBands,
-                        })}
+                        options={chartConfig}
                         ref={chartComponentRef}
                     />
                     {handleMoreClick && (
