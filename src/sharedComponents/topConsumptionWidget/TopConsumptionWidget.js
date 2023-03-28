56c6e4bec271a613dda915ace55fcac8d4717100
remrtehno <remrtehno@mail.ru>
2023-03-28 14:57:57 +0500

    refactoring(PLT-1141): Refine styles add "Explore" btn (#495)

--- src/sharedComponents/topConsumptionWidget/TopConsumptionWidget.js
+++ src/sharedComponents/topConsumptionWidget/TopConsumptionWidget.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 
 import Typography from '../typography';
 import Brick from '../brick';
-import { Link } from 'react-router-dom';
+import { Button } from '../button';
+
 import { TRENDS_BADGE_TYPES, TrendsBadge } from '../trendsBadge';
+import { formatConsumptionValue } from '../../helpers/helpers';
 import { UNITS } from '../../constants/units';
 
+import { ReactComponent as TelescopeSVG } from '../assets/icons/telescope.svg';
 import './TopConsumptionWidget.scss';
-import { Button } from '../button';
-import { formatConsumptionValue } from '../../helpers/helpers';
 
 const TopConsumptionWidget = ({
     subtitle,
@@ -19,56 +20,69 @@ const TopConsumptionWidget = ({
     className = '',
     handleClick,
     widgetType = 'TopConsumptionWidget',
+    exploreBtn,
     style,
 }) => {
     return (
         <div className={`${widgetType}-wrapper ${className}`} style={style}>
-            <div className="d-flex align-items-center justify-content-between mt-1">
+            <div className="d-flex align-items-center justify-content-between">
                 <div>
                     {title && <Typography.Subheader size={Typography.Sizes.md}>{title}</Typography.Subheader>}
+                    <Brick sizeInRem={0.125} />
                     {subtitle && <Typography.Body size={Typography.Sizes.xs}>{subtitle}</Typography.Body>}
                 </div>
+
+                {exploreBtn && (
+                    <Button size={Button.Sizes.md} icon={<TelescopeSVG />} label="Explore" {...exploreBtn} />
+                )}
             </div>
 
-            <Brick sizeInRem={1.5} />
-            <div className={`${widgetType}-table`}>
-                <div className={`${widgetType}-table-row`}>
-                    {heads.map((head, index) => (
-                        <div className={`${widgetType}-table-head-cell`} key={index}>
-                            <Typography.Body size={Typography.Sizes.sm}>{head}</Typography.Body>
-                        </div>
-                    ))}
-                </div>
+            <Brick sizeInRem={0.6875} />
 
-                {rows.map(({ link, label, value, unit, badgePercentage, badgeType,id }) => (
-                    <div className={`${widgetType}-table-row`} key={id}>
-                        <div>
-                            <Button
-                                label={label}
-                                size={Button.Sizes.md}                                
-                                type={Button.Type.link}
-                                className="typography-wrapper link mouse-pointer text-left"
-                                onClick={() => {
-                                    handleClick(label);
-                                }}
-                            />
-                        </div>
-                        <div>
-                            <Typography.Body
-                                className="d-inline mr-1"
-                                size={Typography.Sizes.md}
-                                fontWeight={Typography.Types.SemiBold}>
-                                {formatConsumptionValue(value, 0)}
-                            </Typography.Body>
-                            <Typography.Body className="d-inline" size={Typography.Sizes.xxs}>
-                                {unit}
-                            </Typography.Body>
-                        </div>
-                        <div>
-                            <TrendsBadge type={badgeType} value={badgePercentage} />
-                        </div>
-                    </div>
-                ))}
+            <div className="horizontal-line"></div>
+
+            <Brick sizeInRem={0.6875} />
+
+            <div className={`${widgetType}-table d-block`}>
+                <table className="w-100 top-consumption-widget-table-content">
+                    <tr>
+                        {heads.map((head, index) => (
+                            <td className={`${widgetType}-table-head-cell`} key={index}>
+                                <Typography.Subheader size={Typography.Sizes.sm}>{head}</Typography.Subheader>
+                            </td>
+                        ))}
+                    </tr>
+
+                    {rows.map(({ link, label, value, unit, badgePercentage, badgeType, id }) => (
+                        <tr key={id}>
+                            <td>
+                                <Button
+                                    label={label}
+                                    size={Button.Sizes.md}
+                                    type={Button.Type.link}
+                                    className="typography-wrapper link mouse-pointer text-left p-0"
+                                    onClick={() => {
+                                        handleClick(label);
+                                    }}
+                                />
+                            </td>
+                            <td>
+                                <Typography.Body
+                                    className="d-inline top-consumption-item-value"
+                                    size={Typography.Sizes.md}
+                                    fontWeight={Typography.Types.SemiBold}>
+                                    {formatConsumptionValue(value, 0)}
+                                </Typography.Body>
+                                <Typography.Body className="d-inline gray-550" size={Typography.Sizes.xxs}>
+                                    {unit}
+                                </Typography.Body>
+                            </td>
+                            <td width={40}>
+                                <TrendsBadge type={badgeType} value={badgePercentage} />
+                            </td>
+                        </tr>
+                    ))}
+                </table>
             </div>
         </div>
     );
@@ -90,6 +104,10 @@ TopConsumptionWidget.propTypes = {
             badgeType: PropTypes.oneOf(Object.values(TRENDS_BADGE_TYPES)),
         })
     ).isRequired,
+    exploreBtn: PropTypes.shape({
+        label: PropTypes.string,
+        onClick: PropTypes.func,
+    }),
 };
 
 export default TopConsumptionWidget;
