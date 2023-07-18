88d7fc5c6d9a107ea385c739c4737c62c183af6a
Sudhanshu Rai <sudhanshurai97@gmail.com>
2023-07-18 18:18:56 +0530

    PLT-1237: Scroll Functionality added for tracking to enable Accept Button added.

--- src/pages/auth/TermsAndConditions.js
+++ src/pages/auth/TermsAndConditions.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Typography from '../../sharedComponents/typography';
 import Button from '../../sharedComponents/button/Button';
@@ -13,6 +13,20 @@ import Brick from '../../sharedComponents/brick';
 
 const TermsAndConditions = (props) => {
     const { showModal, closeModal, handleAcceptClick } = props;
+    const [isScrolledToBottom, setIsScrolledToBottom] = useState(false);
+    const modalBodyRef = useRef(null);
+
+    const handleScroll = () => {
+        const { scrollTop, scrollHeight, clientHeight } = modalBodyRef.current;
+        const isBottom = scrollTop + clientHeight >= scrollHeight;
+        setIsScrolledToBottom(isBottom);
+    };
+
+    const acceptButtonDisabled = !isScrolledToBottom;
+
+    useEffect(() => {
+        if (!showModal) setIsScrolledToBottom(false);
+    }, [showModal]);
 
     return (
         <React.Fragment>
@@ -20,7 +34,10 @@ const TermsAndConditions = (props) => {
                 <Modal.Header style={{ padding: '1.5rem' }}>
                     <Typography.Header size={Typography.Sizes.lg}>Accept Terms and Conditions</Typography.Header>
                 </Modal.Header>
-                <Modal.Body style={{ padding: '1.5rem', maxHeight: '20rem', overflowY: 'auto' }}>
+                <Modal.Body
+                    style={{ padding: '1.5rem', maxHeight: '20rem', overflowY: 'auto' }}
+                    ref={modalBodyRef}
+                    onScroll={handleScroll}>
                     <Typography.Body size={Typography.Sizes.md}>{`${TermsAndConditionDescription1}`}</Typography.Body>
                     <Brick sizeInRem={1} />
                     <Typography.Body size={Typography.Sizes.md}>{`${TermsAndConditionDescription2}`}</Typography.Body>
@@ -38,6 +55,7 @@ const TermsAndConditions = (props) => {
                         type={Button.Type.primary}
                         className="btn-width"
                         onClick={handleAcceptClick}
+                        disabled={acceptButtonDisabled}
                     />
                     <Button
                         label="Decline"
