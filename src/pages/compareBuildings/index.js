7d747e93e109f46cc5b834f5db321fc20a7210e8
Sudhanshu Rai <sudhanshurai97@gmail.com>
2023-07-28 13:31:31 +0530

    PLT-1236: Compare Buildings Table units fixes in UI.

--- src/pages/compareBuildings/index.js
+++ src/pages/compareBuildings/index.js
@@ -25,6 +25,9 @@ import { primaryGray1000 } from '../../assets/scss/_colors.scss';
 import { getAverageValue } from '../../helpers/AveragePercent';
 import Brick from '../../sharedComponents/brick';
 import { updateBuildingStore } from '../../helpers/updateBuildingStore';
+import { UserStore } from '../../store/UserStore';
+import { UNITS } from '../../constants/units';
+import { handleUnitConverstion } from '../settings/general-settings/utils';
 
 const SkeletonLoading = () => (
     <SkeletonTheme color={primaryGray1000} height={35}>
@@ -50,15 +53,51 @@ const SkeletonLoading = () => (
     </SkeletonTheme>
 );
 
+const DataTable = ({
+    tableHeader,
+    isLoadingBuildingData,
+    buildingsData,
+    search,
+    handleDownloadCsv,
+    setSearch,
+    totalItemsSearched,
+}) => {
+    return (
+        <DataTableWidget
+            isLoading={isLoadingBuildingData}
+            isLoadingComponent={<SkeletonLoading />}
+            id="compare-building"
+            onSearch={(query) => setSearch(query)}
+            rows={buildingsData}
+            searchResultRows={buildingsData}
+            onDownload={handleDownloadCsv}
+            headers={tableHeader}
+            disableColumnDragging={true}
+            buttonGroupFilterOptions={[]}
+            totalCount={() => {
+                if (search) {
+                    return totalItemsSearched;
+                }
+                return 0;
+            }}
+        />
+    );
+};
+
 const CompareBuildings = () => {
+    const history = useHistory();
+    const { download } = useCSVDownload();
     const [buildingsData, setBuildingsData] = useState([]);
+
     const startDate = DateRangeStore.useState((s) => s.startDate);
     const endDate = DateRangeStore.useState((s) => s.endDate);
     const daysCount = DateRangeStore.useState((s) => +s.daysCount);
+    const userPrefUnits = UserStore.useState((s) => s.unit);
+
     const [sortBy, setSortBy] = useState({});
-    const history = useHistory();
-    const { download } = useCSVDownload();
     const [search, setSearch] = useState('');
+    const [shouldRender, setShouldRender] = useState(true);
+
     const [topEnergyDensity, setTopEnergyDensity] = useState();
     const [totalItemsSearched, setTotalItemsSearched] = useState(0);
 
@@ -66,62 +105,11 @@ const CompareBuildings = () => {
     let entryPoint = '';
     let top = '';
 
-    useEffect(() => {
-        entryPoint = 'entered';
-    }, []);
-
-    useEffect(() => {
-        const updateBreadcrumbStore = () => {
-            BreadcrumbStore.update((bs) => {
-                let newList = [
-                    {
-                        label: 'Compare Buildings',
-                        path: '/energy/compare-buildings',
-                        active: true,
-                    },
-                ];
-                bs.items = newList;
-            });
-            ComponentStore.update((s) => {
-                s.parent = 'portfolio';
-            });
-            updateBuildingStore('portfolio', 'Portfolio', '');
-        };
-        updateBreadcrumbStore();
-    }, []);
-
-    const fetchcompareBuildingsData = async (search, ordered_by = 'energy_density', sort_by) => {
-        setIsLoadingBuildingData(true);
-        let payload = apiRequestBody(startDate, endDate, timeZone);
-        let params = `?ordered_by=${ordered_by}`;
-        if (search) params = params.concat(`&building_search=${encodeURIComponent(search)}`);
-        if (sort_by) params = params.concat(`&sort_by=${sort_by}`);
-        await fetchCompareBuildings(params, payload)
-            .then((res) => {
-                let response = res?.data;
-                let topVal = Math.max(...response.map((o) => o.energy_density));
-                top = Math.max(...response.map((o) => o.energy_density));
-                setTopEnergyDensity(topVal);
-                setBuildingsData(response);
-                setIsLoadingBuildingData(false);
-            })
-            .catch((error) => {
-                setIsLoadingBuildingData(false);
-            });
-    };
-
-    useEffect(() => {
-        const ordered_by = sortBy.name === undefined ? 'energy_density' : sortBy.name;
-        const sort_by = sortBy.method === undefined ? 'dce' : sortBy.method;
-
-        fetchcompareBuildingsData(search, ordered_by, sort_by);
-    }, [search, sortBy, daysCount]);
-
     const renderEnergyDensity = (row) => {
         return (
             <>
                 <Typography.Body size={Typography.Sizes.sm}>
-                    {(row.energy_density / 1000).toFixed(2)} kWh / sq. ft.
+                    {`${(row?.energy_density / 1000).toFixed(2)} ${row?.energy_density_units}`}
                 </Typography.Body>
                 <Brick sizeInRem={0.375} />
                 <TinyBarChart percent={getAverageValue(row.energy_density, 0, top)} />
@@ -181,7 +169,7 @@ const CompareBuildings = () => {
         return <div>{formatConsumptionValue(row.square_footage)}</div>;
     };
 
-    const headerProps = [
+    const [tableHeader, setTableHeader] = useState([
         {
             name: 'Name',
             accessor: 'building_name',
@@ -189,7 +177,7 @@ const CompareBuildings = () => {
             onSort: (method, name) => setSortBy({ method, name }),
         },
         {
-            name: 'Average Consumption / sq. ft.',
+            name: `Average Consumption / ${userPrefUnits === 'si' ? `${UNITS.SQ_M}` : `${UNITS.SQ_FT}`}`,
             accessor: 'energy_density',
             callbackValue: renderEnergyDensity,
 
@@ -208,42 +196,104 @@ const CompareBuildings = () => {
             onSort: (method, name) => setSortBy({ method, name }),
         },
         {
-            name: 'Sq. Ft.',
+            name: `${userPrefUnits === 'si' ? `Sq. M.` : `Sq. Ft.`}`,
             accessor: 'square_footage',
             callbackValue: renderSquareFootage,
             onSort: (method, name) => setSortBy({ method, name }),
         },
-    ];
+    ]);
+
+    const fetchcompareBuildingsData = async (search, ordered_by = 'energy_density', sort_by, userPrefUnits) => {
+        setIsLoadingBuildingData(true);
+        let payload = apiRequestBody(startDate, endDate, timeZone);
+        let params = `?ordered_by=${ordered_by}`;
+        if (search) params = params.concat(`&building_search=${encodeURIComponent(search)}`);
+        if (sort_by) params = params.concat(`&sort_by=${sort_by}`);
+        await fetchCompareBuildings(params, payload)
+            .then((res) => {
+                let response = res?.data;
+                let topVal = Math.max(...response.map((o) => o.energy_density));
+                top = Math.max(...response.map((o) => o.energy_density));
+                response.length !== 0 &&
+                    response.forEach((el) => {
+                        el.square_footage = Math.round(handleUnitConverstion(el.square_footage, userPrefUnits));
+                        el.energy_density_units = `${userPrefUnits === 'si' ? `kWh / sq. m.` : `kWh / sq. ft.`}`;
+                    });
+                setTopEnergyDensity(topVal);
+                setBuildingsData(response);
+                setIsLoadingBuildingData(false);
+            })
+            .catch((error) => {
+                setIsLoadingBuildingData(false);
+            });
+    };
 
     const handleDownloadCsv = async () => {
-        download('Compare_Buildings', getCompareBuildingTableCSVExport(buildingsData, headerProps, topEnergyDensity));
+        download('Compare_Buildings', getCompareBuildingTableCSVExport(buildingsData, tableHeader, topEnergyDensity));
     };
+
+    const updateBreadcrumbStore = () => {
+        BreadcrumbStore.update((bs) => {
+            let newList = [
+                {
+                    label: 'Compare Buildings',
+                    path: '/energy/compare-buildings',
+                    active: true,
+                },
+            ];
+            bs.items = newList;
+        });
+        ComponentStore.update((s) => {
+            s.parent = 'portfolio';
+        });
+        updateBuildingStore('portfolio', 'Portfolio', '');
+    };
+
+    useEffect(() => {
+        if (userPrefUnits) {
+            let newHeaderList = tableHeader;
+            newHeaderList.forEach((record) => {
+                if (record?.accessor === 'energy_density') {
+                    record.name = `Average Consumption / ${
+                        userPrefUnits === 'si' ? `${UNITS.SQ_M}` : `${UNITS.SQ_FT}`
+                    }`;
+                }
+                if (record?.accessor === 'square_footage') {
+                    record.name = `${userPrefUnits === 'si' ? `Sq. M.` : `Sq. Ft.`}`;
+                }
+            });
+            setTableHeader(newHeaderList);
+            setShouldRender((prev) => !prev); // This is set to trigger re-render for DataTableWidget component
+        }
+    }, [userPrefUnits]);
+
+    useEffect(() => {
+        const ordered_by = sortBy.name === undefined ? 'energy_density' : sortBy.name;
+        const sort_by = sortBy.method === undefined ? 'dce' : sortBy.method;
+
+        fetchcompareBuildingsData(search, ordered_by, sort_by, userPrefUnits);
+    }, [search, sortBy, daysCount, userPrefUnits]);
+
+    useEffect(() => {
+        entryPoint = 'entered';
+        updateBreadcrumbStore();
+    }, []);
 
     return (
         <React.Fragment>
             <Header title="Compare Buildings" type="page" />
             <Row className="mt-4">
                 <Col lg={12}>
-                    <DataTableWidget
-                        isLoading={isLoadingBuildingData}
-                        isLoadingComponent={<SkeletonLoading />}
-                        id="compare-building"
-                        onSearch={(query) => {
-                            setSearch(query);
-                        }}
-                        rows={buildingsData}
-                        searchResultRows={buildingsData}
-                        onDownload={() => handleDownloadCsv()}
-                        headers={headerProps}
-                        disableColumnDragging={true}
-                        buttonGroupFilterOptions={[]}
-                        totalCount={() => {
-                            if (search) {
-                                return totalItemsSearched;
-                            }
-
-                            return 0;
-                        }}
+                    <DataTable
+                        tableHeader={tableHeader}
+                        isLoadingBuildingData={isLoadingBuildingData}
+                        buildingsData={buildingsData}
+                        search={search}
+                        sortBy={sortBy}
+                        handleDownloadCsv={handleDownloadCsv}
+                        setSearch={setSearch}
+                        setSortBy={setSortBy}
+                        totalItemsSearched={totalItemsSearched}
                     />
                 </Col>
             </Row>
