f523f0aef8e3722040b5254a4cef9bce33d870e7
Sudhanshu Rai <sudhanshu.rai@sapient.industries>
2023-09-14 15:26:07 +0530

    PLT-1563: Carbon Side Nav fixes.

--- src/pages/carbon/CarbonOverview.js
+++ src/pages/carbon/CarbonOverview.js
@@ -265,7 +265,7 @@ const CarbonOverview = () => {
                 bs.items = newList;
             });
             ComponentStore.update((s) => {
-                s.parent = 'carbon-portfolio';
+                s.parent = 'carbon';
             });
         };
         const updateBuildingData = () => {
