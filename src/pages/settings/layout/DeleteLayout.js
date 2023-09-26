b98ced053a0b3e5b9ffd0602815b19c3d1bfcb46
Sudhanshu Rai <sudhanshu.rai@sapient.industries>
2023-09-26 17:37:43 +0530

    Layout Modal updated.

--- src/pages/settings/layout/DeleteLayout.js
+++ src/pages/settings/layout/DeleteLayout.js
@@ -10,42 +10,37 @@ const DeleteModal = (props) => {
 
     return (
         <Modal show={isModalOpen} backdrop="static" keyboard={false} size="md" centered>
-            <div className="p-4">
+            <Modal.Body className="p-4">
                 <Typography.Header size={Typography.Sizes.lg}>
                     {deleteType === `FLOOR` ? `Delete Floor` : `Delete Space`}
                 </Typography.Header>
 
-                <Brick sizeInRem={2} />
+                <Brick sizeInRem={1.5} />
 
                 <Typography.Body size={Typography.Sizes.md}>
                     {deleteType === `FLOOR`
                         ? `Are you sure you want to delete the Floor and the Spaces it contains?`
                         : ` Are you sure you want to delete the Space and the Spaces it contains?`}
                 </Typography.Body>
+            </Modal.Body>
 
-                <Brick sizeInRem={2.5} />
-
-                <div className="d-flex justify-content-between w-100">
-                    <Button
-                        label={`Cancel`}
-                        size={Button.Sizes.lg}
-                        type={Button.Type.secondaryGrey}
-                        className="w-100"
-                        onClick={onCancel}
-                    />
-
-                    <Button
-                        label={isDeleting ? `Deleting...` : `Delete`}
-                        size={Button.Sizes.lg}
-                        type={Button.Type.primaryDistructive}
-                        className="w-100"
-                        onClick={onSave}
-                        disabled={isDeleting}
-                    />
-                </div>
-
-                <Brick sizeInRem={0.25} />
-            </div>
+            <Modal.Footer className="pb-4 pr-4">
+                <Button
+                    label={`Cancel`}
+                    size={Button.Sizes.lg}
+                    type={Button.Type.secondaryGrey}
+                    className="w-100"
+                    onClick={onCancel}
+                />
+                <Button
+                    label={isDeleting ? `Deleting...` : `Delete`}
+                    size={Button.Sizes.lg}
+                    type={Button.Type.primaryDistructive}
+                    className="w-100"
+                    onClick={onSave}
+                    disabled={isDeleting}
+                />
+            </Modal.Footer>
         </Modal>
     );
 };
