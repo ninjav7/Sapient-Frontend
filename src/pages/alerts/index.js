0b5e31daa3f5f964efd673c2615c57c75cd59743
Sudhanshu Rai <sudhanshu.rai@sapient.industries>
2023-10-16 14:42:24 +0530

    PLT-1672: Alert Page Tabs and Header setup.

--- src/pages/alerts/index.js
+++ src/pages/alerts/index.js
@@ -1,8 +1,68 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Row, Col } from 'reactstrap';
+
+import Typography from '../../sharedComponents/typography';
+import { Button } from '../../sharedComponents/button';
+
 import { BreadcrumbStore } from '../../store/BreadcrumbStore';
 import { ComponentStore } from '../../store/ComponentStore';
 
+import { ReactComponent as PlusSVG } from '../../assets/icon/plus.svg';
+
+import colorPalette from '../../assets/scss/_colors.scss';
+import './styles.scss';
+
+const AlertHeader = (props) => {
+    const { activeTab = false, setActiveTab } = props;
+
+    return (
+        <div className="alerts-header-wrapper d-flex flex-column justify-content-between">
+            <div className="d-flex justify-content-between">
+                <Typography.Header
+                    size={Typography.Sizes.lg}
+                    style={{ color: colorPalette.primaryGray700 }}
+                    className="font-weight-bold">{`Alerts`}</Typography.Header>
+                <div className="d-flex">
+                    <Button label={'Add Alert'} size={Button.Sizes.md} type={Button.Type.primary} icon={<PlusSVG />} />
+                </div>
+            </div>
+
+            <div className="d-flex">
+                <Typography.Header
+                    size={Typography.Sizes.xs}
+                    className={`mouse-pointer mr-4 ${activeTab === 0 ? `active-tab` : ``}`}
+                    style={{ color: colorPalette.primaryGray500 }}
+                    onClick={() => {
+                        setActiveTab(0);
+                    }}>
+                    {`Open Alerts`}
+                </Typography.Header>
+                <Typography.Header
+                    size={Typography.Sizes.xs}
+                    className={`mouse-pointer mr-4 ${activeTab === 1 ? `active-tab` : ``}`}
+                    style={{ color: colorPalette.primaryGray500 }}
+                    onClick={() => {
+                        setActiveTab(1);
+                    }}>
+                    {`Closed Alerts`}
+                </Typography.Header>
+                <Typography.Header
+                    size={Typography.Sizes.xs}
+                    className={`mouse-pointer mr-4 ${activeTab === 2 ? `active-tab` : ``}`}
+                    style={{ color: colorPalette.primaryGray500 }}
+                    onClick={() => {
+                        setActiveTab(2);
+                    }}>
+                    {`Alert Settings`}
+                </Typography.Header>
+            </div>
+        </div>
+    );
+};
+
 const Alerts = () => {
+    const [activeTab, setActiveTab] = useState(0);
+
     const updateBreadcrumbStore = () => {
         BreadcrumbStore.update((bs) => {
             let newList = [
@@ -23,7 +83,15 @@ const Alerts = () => {
         updateBreadcrumbStore();
     }, []);
 
-    return <div>Alerts Page</div>;
+    return (
+        <React.Fragment>
+            <Row>
+                <Col lg={12}>
+                    <AlertHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+                </Col>
+            </Row>
+        </React.Fragment>
+    );
 };
 
 export default Alerts;
