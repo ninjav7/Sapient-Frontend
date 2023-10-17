37a63066f6288a566ddda9428efca641105ad0b6
Sudhanshu Rai <sudhanshu.rai@sapient.industries>
2023-10-17 17:13:24 +0530

    PLT-1672: Alert Building Settings design setup.

--- src/pages/alerts/overall/index.js
+++ src/pages/alerts/overall/index.js
@@ -15,9 +15,9 @@ import { ComponentStore } from '../../../store/ComponentStore';
 import { ReactComponent as PlusSVG } from '../../../assets/icon/plus.svg';
 import { ReactComponent as BuildingTypeSVG } from '../../../sharedComponents/assets/icons/building-type.svg';
 import { ReactComponent as EquipmentTypeSVG } from '../../../sharedComponents/assets/icons/equipment-icon.svg';
+import { ReactComponent as EmailAddressSVG } from '../../../sharedComponents/assets/icons/email-address-icon.svg';
 
-import { alertSettingsHeaderProps } from './constants';
-import { openAlertsMockData } from './mock';
+import { openAlertsMockData, alertSettingsMock } from './mock';
 
 import colorPalette from '../../../assets/scss/_colors.scss';
 import './styles.scss';
@@ -76,7 +76,7 @@ const AlertHeader = (props) => {
     );
 };
 
-const OpenAlerts = ({ handleLastActiveDate }) => {
+const OpenAlerts = () => {
     const [openAlertsList, setOpenAlertsList] = useState(openAlertsMockData);
     const [openAlertsCount, setOpenAlertListsCount] = useState(0);
 
@@ -177,7 +177,7 @@ const OpenAlerts = ({ handleLastActiveDate }) => {
     );
 };
 
-const ClosedAlerts = ({ handleLastActiveDate }) => {
+const ClosedAlerts = () => {
     const [closedAlertsList, setClosedAlertsList] = useState(openAlertsMockData);
     const [closedAlertsCount, setClosedAlertListsCount] = useState(0);
 
@@ -279,13 +279,58 @@ const ClosedAlerts = ({ handleLastActiveDate }) => {
 };
 
 const AlertSettings = () => {
-    const [alertSettingsList, setAlertSettingsList] = useState([]);
+    const [alertSettingsList, setAlertSettingsList] = useState(alertSettingsMock);
     const [alertSettingsCount, setAlertSettingsCount] = useState(0);
 
     const [pageNo, setPageNo] = useState(1);
     const [pageSize, setPageSize] = useState(10);
 
-    const [checkedAll, setCheckedAll] = useState(false);
+    const renderAlertType = (row) => {
+        return (
+            <div className="d-flex align-items-center" style={{ gap: '0.75rem' }}>
+                {row?.target_type === 'Building' ? (
+                    <BuildingTypeSVG className="p-0 square" />
+                ) : (
+                    <EquipmentTypeSVG className="p-0 square" />
+                )}
+                <Typography.Body size={Typography.Sizes.lg} style={{ color: colorPalette.primaryGray700 }}>
+                    {`${row?.target}`}
+                </Typography.Body>
+            </div>
+        );
+    };
+
+    const renderSentAddress = (row) => {
+        return (
+            <div>
+                <div className="d-flex align-items-center" style={{ gap: '0.50rem' }}>
+                    <EmailAddressSVG className="p-0 square" />
+                    <Typography.Subheader
+                        size={Typography.Sizes.md}
+                        style={{ color: colorPalette.primaryGray600, fontWeight: 500 }}>
+                        {`Email Address`}
+                    </Typography.Subheader>
+                </div>
+
+                <Typography.Body size={Typography.Sizes.lg} style={{ color: colorPalette.primaryGray500 }}>
+                    {`${row?.sent_to}`}
+                </Typography.Body>
+                <Typography.Body size={Typography.Sizes.sm} style={{ color: colorPalette.primaryGray400 }}>
+                    {`Send if condition lasts 0 min, resend every 60 mins`}
+                </Typography.Body>
+            </div>
+        );
+    };
+
+    const renderConditions = (row) => {
+        return (
+            <div style={{ maxWidth: '15vw' }}>
+                <Typography.Body size={Typography.Sizes.lg} style={{ color: colorPalette.primaryGray500 }}>
+                    {`${row?.condition}`}
+                </Typography.Body>
+            </div>
+        );
+    };
 
     const currentRow = () => {
         return alertSettingsList;
@@ -303,14 +348,42 @@ const AlertSettings = () => {
                 rows={currentRow()}
                 disableColumnDragging={true}
                 searchResultRows={currentRow()}
-                headers={alertSettingsHeaderProps}
+                headers={[
+                    {
+                        name: 'Target',
+                        accessor: 'target',
+                        callbackValue: renderAlertType,
+                    },
+                    {
+                        name: 'Target Type',
+                        accessor: 'target_type',
+                    },
+                    {
+                        name: 'Condition',
+                        accessor: 'condition',
+                        callbackValue: renderConditions,
+                    },
+                    {
+                        name: 'Sent To',
+                        accessor: 'sent_to',
+                        callbackValue: renderSentAddress,
+                    },
+                    {
+                        name: 'Created',
+                        accessor: 'created_at',
+                    },
+                ]}
                 filterOptions={[]}
                 currentPage={pageNo}
                 onChangePage={setPageNo}
                 pageSize={pageSize}
                 onPageSize={setPageSize}
-                onDeleteRow={() => alert('Delete record')}
-                isDeletable={true}
+                onDeleteRow={(row) => (row) => {
+                    return true;
+                }}
+                isDeletable={(row) => {
+                    return true;
+                }}
                 totalCount={(() => {
                     return alertSettingsCount;
                 })()}
@@ -324,23 +397,6 @@ const Alerts = () => {
 
     const userPrefDateFormat = UserStore.useState((s) => s.dateFormat);
     const userPrefTimeFormat = UserStore.useState((s) => s.timeFormat);
-
-    function isValidDate(d) {
-        return d instanceof Date && !isNaN(d);
-    }
-
-    const handleLastActiveDate = (last_login) => {
-        let dt = '';
-        if (isValidDate(new Date(last_login)) && last_login != null) {
-            const last_dt = new Date(last_login);
-            const dateFormat = userPrefDateFormat === `DD-MM-YYYY` ? `D MMM` : `MMM D`;
-            const timeFormat = userPrefTimeFormat === `12h` ? `hh:mm A` : `HH:mm`;
-            dt = moment.utc(last_dt).format(`${dateFormat} 'YY @ ${timeFormat}`);
-        } else {
-            dt = 'Never';
-        }
-        return dt;
-    };
 
     const handleTabSwitch = (event) => {
         setActiveTab(+event.target.id);
@@ -376,8 +432,8 @@ const Alerts = () => {
 
             <Row>
                 <Col lg={12}>
-                    {activeTab === 0 && <OpenAlerts handleLastActiveDate={handleLastActiveDate} />}
-                    {activeTab === 1 && <ClosedAlerts handleLastActiveDate={handleLastActiveDate} />}
+                    {activeTab === 0 && <OpenAlerts />}
+                    {activeTab === 1 && <ClosedAlerts />}
                     {activeTab === 2 && <AlertSettings />}
                 </Col>
             </Row>
--- src/pages/alerts/overall/mock.js
+++ src/pages/alerts/overall/mock.js
@@ -6,7 +6,7 @@ export const openAlertsMockData = [
         timestamps: '2023-10-17T07:00:08.657Z',
     },
     {
-        target: 'Cooling - Multiple Equipment',
+        target: 'Cooling - Multiple equipment',
         target_type: 'Equipment',
         condition: 'RMS Current is above 123 A',
         timestamps: '2023-10-17T08:00:08.657Z',
@@ -22,5 +22,50 @@ export const openAlertsMockData = [
         target_type: 'Equipment',
         condition: 'Energy consumption for the month is above 10,000 kWh',
         timestamps: '2023-10-17T10:25:08.657Z',
+    },
+];
+
+export const alertSettingsMock = [
+    {
+        target: 'Building of any type',
+        target_type: 'Building',
+        condition: 'Energy consumption for the month is above 400 kWh',
+        sent_to: 'test.user@sapient.industries',
+        created_at: '2023/10/04',
+    },
+    {
+        target: 'Cooling - Multiple equipment',
+        target_type: 'Equipment',
+        sent_to: 'test.user@sapient.industries',
+        condition: 'RMS Current is above 123 A',
+        created_at: '2023/10/05',
+    },
+    {
+        target: 'Equipment of any type',
+        target_type: 'Equipment',
+        condition: 'Peak Power is above 123 kW',
+        sent_to: 'test.user@sapient.industries',
+        created_at: '2023/10/06',
+    },
+    {
+        target: '15 University Blvd. Hartford, CT',
+        target_type: 'Building',
+        sent_to: 'test.user@sapient.industries',
+        condition: 'Peak Demand for current month is above 1300 kWh',
+        created_at: '2023/10/07',
+    },
+    {
+        target: '123 Main St. Portland, OR',
+        target_type: 'Building',
+        sent_to: 'test.user@sapient.industries',
+        condition: 'Energy consumption for the month is above 400 kWh',
+        created_at: '2023/10/08',
+    },
+    {
+        target: 'Cooling AHU 1',
+        target_type: 'Equipment',
+        sent_to: 'test.user@sapient.industries',
+        condition: 'Peak Power is above 123 kWh',
+        created_at: '2023/10/09',
     },
 ];
