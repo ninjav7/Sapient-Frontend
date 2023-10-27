5bfb68570d2eae2363975fdb0f39a627b8b0e325
Sudhanshu Rai <sudhanshu.rai@sapient.industries>
2023-10-27 11:34:31 +0530

    Panel Configuration functionality updated to includes Amps & Voltage change.

--- src/pages/settings/panels/PanelConfiguration.js
+++ src/pages/settings/panels/PanelConfiguration.js
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
-import Typography from '../../../sharedComponents/typography';
+
+import { UserStore } from '../../../store/UserStore';
+
 import Brick from '../../../sharedComponents/brick';
 import { Button } from '../../../sharedComponents/button';
+import Select from '../../../sharedComponents/form/select';
+import Typography from '../../../sharedComponents/typography';
 import InputTooltip from '../../../sharedComponents/form/input/InputTooltip';
-import { UserStore } from '../../../store/UserStore';
+
 import { updatePanelDetails } from './services';
 
+import { voltsOption } from './utils';
+import { compareObjData } from '../../../helpers/helpers';
+
 const PanelConfiguration = (props) => {
-    const { showPanelConfigModal = false, closePanelConfigModal, panelObj, bldgId, fetchSinglePanelData } = props;
+    const {
+        showPanelConfigModal = false,
+        closePanelConfigModal,
+        panelObj,
+        bldgId,
+        fetchSinglePanelData,
+        fetchBreakersData,
+    } = props;
 
     const [panelData, setPanelData] = useState({});
     const [isProcessing, setIsProcessing] = useState(false);
@@ -17,22 +31,28 @@ const PanelConfiguration = (props) => {
         if (!panelData?.panel_id) return;
 
         setIsProcessing(true);
-        const params = `?panel_id=${panelData?.panel_id}`;
 
-        await updatePanelDetails(params, { rated_amps: panelData?.rated_amps })
+        const params = `?panel_id=${panelData?.panel_id}`;
+        let payload = {};
+
+        if (panelObj?.rated_amps !== panelData?.rated_amps) payload.rated_amps = panelData?.rated_amps;
+        if (panelObj?.voltage !== panelData?.voltage) payload.voltage = panelData?.voltage;
+
+        await updatePanelDetails(params, payload)
             .then((res) => {
                 const response = res?.data;
                 if (response?.success) {
                     UserStore.update((s) => {
                         s.showNotification = true;
-                        s.notificationMessage = 'Rated Amps for Panel updated successfully.';
+                        s.notificationMessage = 'Panel configuration updated successfully.';
                         s.notificationType = 'success';
                     });
                     fetchSinglePanelData(panelData?.panel_id, bldgId);
+                    fetchBreakersData(panelData?.panel_id, bldgId);
                 } else {
                     UserStore.update((s) => {
                         s.showNotification = true;
-                        s.notificationMessage = 'Failed to update Rated Amps for Panel due to Internal Server Error.';
+                        s.notificationMessage = 'Failed to update Panel configuration due to Internal Server Error.';
                         s.notificationType = 'error';
                     });
                 }
@@ -40,7 +60,7 @@ const PanelConfiguration = (props) => {
             .catch(() => {
                 UserStore.update((s) => {
                     s.showNotification = true;
-                    s.notificationMessage = 'Unable to update Rated Amps for Panel due to Internal Server Error.';
+                    s.notificationMessage = 'Unable to update Panel configuration due to Internal Server Error.';
                     s.notificationType = 'error';
                 });
             })
@@ -74,6 +94,22 @@ const PanelConfiguration = (props) => {
                     value={panelData?.rated_amps}
                 />
 
+                <Brick sizeInRem={1.5} />
+
+                <div className="w-100">
+                    <Typography.Body size={Typography.Sizes.md}>Volts</Typography.Body>
+                    <Brick sizeInRem={0.25} />
+                    <Select
+                        placeholder="Select Volts"
+                        options={voltsOption}
+                        currentValue={voltsOption.filter((option) => option.value === panelData?.voltage)}
+                        onChange={(e) => {
+                            setPanelData({ ...panelData, voltage: e.value });
+                        }}
+                        isSearchable={false}
+                    />
+                </div>
+
                 <Brick sizeInRem={2.5} />
 
                 <div className="d-flex justify-content-between w-100">
@@ -93,7 +129,7 @@ const PanelConfiguration = (props) => {
                         size={Button.Sizes.lg}
                         type={Button.Type.primary}
                         className="w-100"
-                        disabled={panelObj?.rated_amps === panelData?.rated_amps || isProcessing}
+                        disabled={compareObjData(panelObj, panelData) || isProcessing}
                         onClick={handlePanelUpdate}
                     />
                 </div>
