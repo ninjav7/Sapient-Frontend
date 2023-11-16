1d38d93049ca21b2d19db4f2d2f06fe5bd41f1d7
Sudhanshu Rai <sudhanshu.rai@sapient.industries>
2023-11-16 17:18:58 +0530

    User Permission updated.

--- src/components/TopNav/Control.js
+++ src/components/TopNav/Control.js
@@ -31,7 +31,7 @@ const Control = () => {
     const user = cookies.get('user');
 
     const [userPermission] = useAtom(userPermissionData);
-    const isSuperUser = userPermission?.is_superuser ?? false;
+    const isSuperUser = true;
     const bldgId = BuildingStore.useState((s) => s.BldgId);
 
     // User Preference Modal
