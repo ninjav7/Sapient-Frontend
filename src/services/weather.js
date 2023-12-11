403403aa5cdecdaf6b95d957c8c5c993cf9c3fc0
Oleksii-Poletaiev <oleksii.poletaiev@sapient.industries>
2023-12-11 17:53:36 +0100

    Added res.data to the request

--- src/services/weather.js
+++ src/services/weather.js
@@ -11,5 +11,5 @@ export function getWeatherData(obj) {
 
     // return response;
     const params = `?building_id=${obj?.bldg_id}&date_from=${obj?.date_from}&date_to=${obj?.date_to}&tz_info=${obj?.tz_info}&aggregate=${obj?.range}`;
-    return axiosInstance.get(`${getWeather}${params}`).then((res) => res);
+    return axiosInstance.get(`${getWeather}${params}`).then((res) => res.data);
 }
