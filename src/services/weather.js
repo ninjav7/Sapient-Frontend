97b69332d951a3ae632774c082591cde3bb7fe86
Oleksii-Poletaiev <oleksii.poletaiev@sapient.industries>
2023-12-12 21:14:27 +0100

    On the Total Energy Consumption, fixed high and low temp, added right names for exported CSV instead of series 2, etc. and added name to the exported CSV file

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,7 +1,17 @@
 import axiosInstance from './axiosInstance';
 import { getWeather } from './Network';
+// import dataWeatherHourMock from './dataWeatherHourMock.json';
+// import dataWeatherDayMock from './dataWeatherDayMock.json';
 
 export function getWeatherData(obj) {
+    // return new Promise((res) => {
+    //     if (obj.range === 'hour') {
+    //         res(dataWeatherHourMock.data);
+    //     } else {
+    //         res(dataWeatherDayMock.data);
+    //     }
+    // });
+
     const params = `?building_id=${obj?.bldg_id}&date_from=${obj?.date_from}&date_to=${obj?.date_to}&tz_info=${obj?.tz_info}&aggregate=${obj?.range}`;
 
     return axiosInstance.get(`${getWeather}${params}`).then((res) => res.data);
