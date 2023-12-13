cbdcbb2c816e722827f1580c26c6d284058c9d98
Oleksii-Poletaiev <oleksii.poletaiev@sapient.industries>
2023-12-13 18:26:56 +0100

    Added weather to the use end charts too

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,18 +1,18 @@
 import axiosInstance from './axiosInstance';
 import { getWeather } from './Network';
 // Should be enabled only in localhost
-// import dataWeatherHourMock from './dataWeatherHourMock.json';
-// import dataWeatherDayMock from './dataWeatherDayMock.json';
+import dataWeatherHourMock from './dataWeatherHourMock.json';
+import dataWeatherDayMock from './dataWeatherDayMock.json';
 
 export function getWeatherData(obj) {
     // Should be enabled only in localhost
-    // return new Promise((res) => {
-    //     if (obj.range === 'hour') {
-    //         res(dataWeatherHourMock.data);
-    //     } else {
-    //         res(dataWeatherDayMock.data);
-    //     }
-    // });
+    return new Promise((res) => {
+        if (obj.range === 'hour') {
+            res(dataWeatherHourMock.data);
+        } else {
+            res(dataWeatherDayMock.data);
+        }
+    });
 
     const params = `?building_id=${obj?.bldg_id}&date_from=${obj?.date_from}&date_to=${obj?.date_to}&tz_info=${obj?.tz_info}&aggregate=${obj?.range}`;
 
