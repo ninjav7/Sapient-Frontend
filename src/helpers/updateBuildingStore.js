febaa380f86ad7e14a6924cd1557acc33960e8e9
Sudhanshu Rai <sudhanshu.rai@sapient.industries>
2023-12-21 13:31:28 +0530

    Building Store UI handle fixes.

--- src/helpers/updateBuildingStore.js
+++ src/helpers/updateBuildingStore.js
@@ -1,16 +1,19 @@
 import { BuildingStore } from '../store/BuildingStore';
 import { timeZone } from '../utils/helper';
 
-export const updateBuildingStore = (bldgId, bldgName, bldgTimeZone, isPlugOnly) => {
+export const updateBuildingStore = (bldgId, bldgName, bldgTimeZone, isPlugOnly = '') => {
+    const buildingIsPlugOnly = isPlugOnly.toString();
+    const buildingTimeZone = bldgTimeZone === '' ? timeZone : bldgTimeZone;
+
     localStorage.setItem('buildingId', bldgId);
     localStorage.setItem('buildingName', bldgName);
-    localStorage.setItem('buildingTimeZone', bldgTimeZone === '' ? timeZone : bldgTimeZone);
-    localStorage.setItem('isPlugOnly', isPlugOnly);
+    localStorage.setItem('buildingTimeZone', buildingTimeZone);
+    localStorage.setItem('isPlugOnly', buildingIsPlugOnly);
 
     BuildingStore.update((s) => {
         s.BldgId = bldgId;
         s.BldgName = bldgName;
-        s.BldgTimeZone = bldgTimeZone === '' ? timeZone : bldgTimeZone;
-        s.isPlugOnly = isPlugOnly;
+        s.BldgTimeZone = buildingTimeZone;
+        s.isPlugOnly = buildingIsPlugOnly;
     });
 };
