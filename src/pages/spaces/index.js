7421ec4c7a58c10c3b60f0d969d45f896767df5d
Oleksii-Poletaiev <oleksii.poletaiev@sapient.industries>
2024-02-14 12:50:39 +0100

    fixed code

--- src/pages/spaces/index.js
+++ src/pages/spaces/index.js
@@ -14,7 +14,6 @@ import { xaxisLabelsCount, xaxisLabelsFormat } from '../../sharedComponents/help
 import { updateBuildingStore } from '../../helpers/updateBuildingStore';
 import { fetchEnergyConsumptionBySpaceDataHelper } from '../../components/energyConsumptionBySpace/helpers';
 import SpacesListTable from './SpacesListTable';
-import SpaceDetailsPage from './spaceDetailsPage';
 
 const Spaces = () => {
     const { bldgId } = useParams();
@@ -155,8 +154,6 @@ const Spaces = () => {
             <Brick sizeInRem={1.5} /> */}
 
             <SpacesListTable colorfulSpaces={spacesData} />
-
-            {/* <SpaceDetailsPage /> */}
         </>
     );
 };
