c5dd3b2a0b0a8e216786a871a8f6837a6c0ad038
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-02-28 15:41:58 +0530

    Base URL updated.

--- src/services/Network.js
+++ src/services/Network.js
@@ -1,4 +1,4 @@
-export const BaseUrl = 'https://sapient-dev-service.stacksapien.com';
+export const BaseUrl = 'http://sapient-energy-service-dev.azurewebsites.net';
 export const SingularityBaseUrl = 'https://api.singularity.energy/v2/';
 
 // Auth
