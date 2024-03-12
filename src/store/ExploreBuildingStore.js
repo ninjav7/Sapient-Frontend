87e5fcfb511e6db64c8f776a4bc5041d2011b22f
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-03-12 14:52:59 +0530

    Explore Cleanup.

--- src/store/ExploreBuildingStore.js
+++
@@ -1,7 +0,0 @@
-import { Store } from 'pullstate';
-
-export const ExploreBuildingStore = new Store({
-    exploreBldId: localStorage.getItem('exploreBldId'),
-    exploreBldName: localStorage.getItem('exploreBldName'),
-    exploreBldTimeZone: localStorage.getItem('exploreBldTimeZone'),
-});
