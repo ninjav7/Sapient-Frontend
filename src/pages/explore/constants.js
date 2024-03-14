d00726453e0aab040f4f3d4c5980ec2b6e54a62c
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-03-14 16:58:52 +0530

    List updated.

--- src/pages/explore/constants.js
+++ src/pages/explore/constants.js
@@ -1,6 +1,5 @@
 export const EXPLORE_FILTER_TYPE = Object.freeze({
     NO_GROUPING: 'no-grouping',
-    BY_ENDUSE: 'by-enduse',
     BY_SPACE: 'by-space',
     BY_SPACE_TYPE: 'by-spacetype',
     BY_FLOOR: 'by-floor',
@@ -20,17 +19,12 @@ export const exploreFiltersList = [
         label: `By Space Type`,
         value: EXPLORE_FILTER_TYPE.BY_SPACE_TYPE,
     },
-    // PLT-2039: Planned to enable in Future
-    // {
-    //     label: `By End Use`,
-    //     value: EXPLORE_FILTER_TYPE.BY_ENDUSE,
-    // },
-    // {
-    //     label: `By Floor`,
-    //     value: EXPLORE_FILTER_TYPE.BY_FLOOR,
-    // },
-    // {
-    //     label: `By Equipment Type`,
-    //     value: EXPLORE_FILTER_TYPE.BY_EQUIPMENT_TYPE,
-    // },
+    {
+        label: `By Floor`,
+        value: EXPLORE_FILTER_TYPE.BY_FLOOR,
+    },
+    {
+        label: `By Equipment Type`,
+        value: EXPLORE_FILTER_TYPE.BY_EQUIPMENT_TYPE,
+    },
 ];
